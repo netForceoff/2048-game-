4c5dc220778ad7954e01aeefffc70a0a8f66751a
Sergey Krasnoyarov <krasnoyarovsergei@gmail.com>
2018-12-20 09:53:32 +0500

    adding movement, addition cells and animation

--- cell.js
+++ cell.js
@@ -1,27 +1,73 @@
 class Cell {
-  constructor(parentElement, game) {
-    this.game = game;
+    constructor(fieldElement, game) {
+        this.game = game;
+        this.fieldElement = fieldElement;
 
-    this.element = createElementAndAppend('cells', parentElement);
+        this.element = createElementAndAppend('cells', fieldElement);
 
-    this.element.style.width = this.game.cellSize + 'vmin';
-    this.element.style.height = this.game.cellSize + 'vmin';
+        this.element.style.width = this.game.cellSize + 'vmin';
+        this.element.style.height = this.game.cellSize + 'vmin';
 
-      if (Math.random() > 0.8) {
-        this.spawn();
+        if (Math.random() > 0.8) {
+             this.spawn();
+        }
+
+        //this.element.onclick = this.stackCells.bind(this) // bind(this) -  Будет выполняться в контексте текущего объекта, а не в window
+    }
+
+    get value() { // Добавляем get и set, потому что будет отрисовка
+        return this._value || 0;
+    }
+
+    set value(value) {
+        this._value = value;
+        this.element.innerHTML = value == 0 ? '' : value;
+        this.element.setAttribute('data-color', value);
+    }
+
+    clear() {
+        this.value = '';
+    }
+
+    stackCells(cell) {
+      if (this.value) {
+        this.game.createScore(this.value + cell.value);
       }
-  }
 
-  set value(value) {
-    this._value = value;
-    this.element.innerHTML = value == 0 ? '' : value;
-  }
+      new AnimateCell(cell, this);
 
-  get value() {
-    return this._value || 0;
-  }
+      this.value += cell.value;
+      cell.clear();
+    }
 
-  spawn() {
-    this.value = Math.random() > 0.5 ? 4 : 2;
+    isSameTo(cell) {
+      return this.value == cell.value;
+    }
+
+    spawn() {
+        this.value = Math.random() > 0.5 ? 4 : 2;
+    }
+
+    get isEmpty() {
+        return this.value == 0;
+    }
+}
+
+class AnimateCell {
+  constructor(fromCell, toCell) {
+    this.element = fromCell.element.cloneNode(true);
+    this.element.className = 'cells animate';
+
+    this.element.style.top = (fromCell.element.offsetTop - 10) + 'px';
+    this.element.style.left = (fromCell.element.offsetLeft - 10) + 'px';
+
+    fromCell.fieldElement.appendChild(this.element);
+
+    this.element.style.top = (toCell.element.offsetTop - 10) + 'px';
+    this.element.style.left = (toCell.element.offsetLeft - 10) + 'px';
+
+    setTimeout(function() {
+      fromCell.fieldElement.removeChild(this.element)
+    }.bind(this), 1000);
   }
 }
--- game.js
+++ game.js
@@ -1,44 +1,162 @@
 class Game {
-  constructor(parentElement, size) {
-    this.size = size;
-    this.fieldSize = 80;
+    constructor(parentElement, size) {
+      this.size = size;
 
-    this.cellSize = this.fieldSize/this.size - 2;
+      this.fieldSize = 80;
+      this.cellSize = this.fieldSize/this.size - 2;
 
-    let gameFieldElement = createElementAndAppend('game', parentElement);
-    this.headerElement = createElementAndAppend('header', gameFieldElement);
-    this.scoreElement = createElementAndAppend('score', this.headerElement);
+      let gameFieldElement = createElementAndAppend('game', parentElement);
+      this.headerElement = createElementAndAppend('header', gameFieldElement);
+      this.scoreElement = createElementAndAppend('score', this.headerElement);
 
-    let fieldElement = createElementAndAppend('field', gameFieldElement);
+      this.score = 0;
 
-    this.score = 0;
+      let fieldElement = createElementAndAppend('field', gameFieldElement);
 
-    this.field = [];
+      this.field = [];
 
-    for (let i = 0; i < this.size; i++) {
-      this.field[i] = [];
-      for (let k = 0; k < this.size; k++) {
-        this.field[i][k] = new Cell(fieldElement, this);
-      }
-    }
-
-
-  }
-
-  spawnUnit() {
-    let spawnCells = [];
-    for (let i = 0; i < this.field.length; i++) {
-      for (let k = 0; k < this.field[i].length; k++) {
-        if(!this.field[i][k].value) {
-          spawnCells.push(this.field[i][k]);
+      for (let i = 0; i < size; i ++) {
+          this.field[i] = [];
+        for (let k = 0; k < size; k++) {
+         this.field[i][k] = new Cell(fieldElement, this);
         }
       }
+
+     window.onkeyup = e => {
+       switch (e.keyCode) {
+         case 38:
+           this.moveUp();
+           break;
+         case 40:
+            this.moveDown();
+            break;
+         case 39:
+            this.moveRight();
+           break;
+         case 37:
+            this.moveLeft();
+            break;
+
+       }
+      };
     }
-    if (spawnCells.length) {
-      spawnCells[getRandomInt(0, spawnCells.length - 1)].spawn()
+
+    set score(value) {
+      this._score = value;
+      this.scoreElement.innerHTML = 'Score: ' + value;
     }
-    else {
-      alert('Проеб!');
+
+    get score() {
+      return this._score;
     }
+
+
+
+    spawnUnit() {
+        let emptyCells = [];
+        for (let i = 0; i <this.field.length; i++) {
+            for (let k = 0; k < this.field[i].length; k++) {
+                if (!this.field[i][k].value) {
+                    emptyCells.push(this.field[i][k]);
+                }
+            }
+        }
+
+        if (emptyCells.length) {
+            emptyCells[getRandomInt(0, emptyCells.length - 1)].spawn();
+        } else {
+            alert('Проеб!');
+        }
+
+    }
+
+    createScore(value) {
+      this.score += value;
+    }
+    //Обходим все ячейки в противоположном направлении от нажатия клавиши
+    // По направлею первую строку или столбец пропускаем
+    // Для ячейки ищем следующую заняткую ячейку (или последнюю)
+    //Если ячейка занята и совпадает с нашей, то объединяем
+    //Если ячейка занята и не совпадает, то проверяем предыдущую ячейку
+    moveRight() {
+        let hasMoved = false;
+        for (let i = 0; i < this.size; i++) {
+            for (let k = this.size - 2; k >=0; k--) {
+              hasMoved = this.move(i, k, false, true, this.isLastKey.bind(this)) || hasMoved;
+                }
+            }
+            this.checkMove(hasMoved);
+    }
+
+    moveLeft() {
+        let hasMoved = false;
+        for (let i = 0; i < this.size; i++) {
+            for (let k = 1; k < this.size; k++) {
+              hasMoved = this.move(i, k, false, false, this.isFirstKey.bind(this)) || hasMoved;
+                }
+            }
+            this.checkMove(hasMoved);
+    }
+
+    moveDown() {
+      let hasMoved = false;
+      for (let i = this.size -2; i >= 0; i--) {
+      for (let k = 0; k < this.size; k++) {
+          hasMoved = this.move(i, k, true, true, this.isLastKey.bind(this)) || hasMoved;
+            }
+        }
+        this.checkMove(hasMoved);
+    }
+
+    moveUp() {
+      let hasMoved = false;
+      for (let i = 1; i < this.size; i++) {
+        for (let k = 0; k < this.size; k++) {
+          hasMoved = this.move(i, k, true, false, this.isFirstKey.bind(this)) || hasMoved;
+            }
+        }
+        this.checkMove(hasMoved);
+    }
+
+    checkMove(elem) {
+      if (elem) {
+        this.spawnUnit();
+      }
+    }
+
+    move(i, k, isI, isIncrement, keyCheck) {
+      let hasMoved = false;
+
+      let inc = isIncrement ? 1 : -1;
+      let currentCell = this.field[i][k];
+        if (currentCell.isEmpty) {
+          return hasMoved;
+          }
+
+        let nextCellKey = (isI ? i : k) + inc;
+        while (nextCellKey < this.size && nextCellKey >= 0) {
+          let nextCell = this.field[isI ? nextCellKey : i][isI ? k : nextCellKey];
+          if (!nextCell.isEmpty || keyCheck(nextCellKey)) {
+            if ((nextCell.isEmpty && keyCheck(nextCellKey)) || nextCell.isSameTo(currentCell)) {
+              this.field[isI ? nextCellKey : i][isI ? k : nextCellKey].stackCells(currentCell);
+              hasMoved = true;
+            } else if (!nextCell.isEmpty && ((isI && (nextCellKey + (inc * -1) != i)) || (!isI && (nextCellKey + (inc * -1) != k)))) {
+                this.field[isI ? nextCellKey + (inc * -1) : i][isI ? k : nextCellKey + (inc * -1)].stackCells(currentCell);
+                hasMoved = true;
+            }
+              break;
+            }
+              nextCellKey += inc;
+              nextCell = this.field[isI ? nextCellKey : i][isI ? k : nextCellKey];
+            }
+            return hasMoved;
+    }
+
+    isLastKey(key) {
+     return key == (this.size - 1);
+   }
+
+   isFirstKey(key) {
+     return key == 0;
+   }
   }
-}
